519b1bb9230385821dd1d9544b03c3c555c99282
José Augusto de Araújo Nascimento <joseaugustodearaujonascimento@gmail.com>
2012-08-27 09:38:53 -0300

    Adicionando drag&drop.

--- android.chess/src/eng/android/chess/views/Tabuleiro.java
+++ android.chess/src/eng/android/chess/views/Tabuleiro.java
@@ -10,113 +10,129 @@ import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.DragEvent;
 import android.view.View;
 
 /**
  * @author augusteiner
  *
  */
-public class Tabuleiro extends View
-{
+public class Tabuleiro extends View {
 
-    private Rect tPlaceRect;
-    private Paint tPaint;
-    private int tPlaceSide;
+	private Rect tPlaceRect;
+	private Paint tPaint;
+	private int tPlaceSide;
 
-    /**
-     * @param context
-     */
-    public Tabuleiro(Context context)
-    {
-        super(context);
+	/**
+	 * @param context
+	 */
+	public Tabuleiro(Context context) {
+		super(context);
 
-        initTabuleiro();
-    }
+		initTabuleiro();
+	}
 
-    /**
-     * @param context
-     * @param attrs
-     */
-    public Tabuleiro(Context context, AttributeSet attrs)
-    {
-        super(context, attrs);
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public Tabuleiro(Context context, AttributeSet attrs) {
+		super(context, attrs);
 
-        initTabuleiro();
-    }
+		initTabuleiro();
+	}
 
-    /**
-     * @param context
-     * @param attrs
-     * @param defStyle
-     */
-    public Tabuleiro(Context context, AttributeSet attrs, int defStyle)
-    {
-        super(context, attrs, defStyle);
+	/**
+	 * @param context
+	 * @param attrs
+	 * @param defStyle
+	 */
+	public Tabuleiro(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
 
-        initTabuleiro();
-    }
+		initTabuleiro();
+	}
 
-    /**
-     * Prepara objetos para desenho deste tabuleiro.
-     */
-    protected void initTabuleiro()
-    {
-        tPlaceRect = new Rect();
-        tPaint = new Paint();
-    }
+	/**
+	 * Prepara objetos para desenho deste tabuleiro.
+	 */
+	protected void initTabuleiro() {
+		tPlaceRect = new Rect();
+		tPaint = new Paint();
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see android.view.View#onMeasure(int, int)
-     */
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
-    {
-        setMeasuredDimension(getLayoutParams().width + 1,
-            getLayoutParams().height + 2);
-    }
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see android.view.View#onMeasure(int, int)
+	 */
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		setMeasuredDimension(getLayoutParams().width + 1,
+				getLayoutParams().height + 2);
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see android.view.View#onDraw(android.graphics.Canvas)
-     */
-    @Override
-    protected void onDraw(Canvas canvas)
-    {
-        super.onDraw(canvas);
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see android.view.View#onDragEvent(android.view.DragEvent)
+	 */
+	@Override
+	public boolean onDragEvent(DragEvent event) {
+		switch (event.getAction()) {
+			case DragEvent.ACTION_DROP :
+				Peca p = (Peca)event.getLocalState();
 
-        tPlaceSide = getLayoutParams().width / 8;
-        tPaint.setColor(Color.BLACK);
-        tPaint.setStyle(Style.FILL);
+				if (p != null){
+					//
 
-        for (int i = 0, j; i < 8; i++)
-        {
-            for (j = i % 2; j < 8; j += 2)
-            {
-                tPlaceRect.set(i * tPlaceSide,// left,
-                    j * tPlaceSide,// top,
-                    (i + 1) * tPlaceSide,// right,
-                    (j + 1) * tPlaceSide// bottom
-                );
+					return true;
+				}
 
-                canvas.drawRect(tPlaceRect, tPaint);
-            }
-        }
+				break;
+			default :
+				return false;
+		}
 
-        tPlaceRect.set(0, 0, getLayoutParams().width, getLayoutParams().height);
-        tPaint.setStyle(Style.STROKE);
-        canvas.drawRect(tPlaceRect, tPaint);
-    }
+		// TODO Auto-generated method stub
+		return super.onDragEvent(event);
+	}
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see android.view.View#onDraw(android.graphics.Canvas)
+	 */
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
 
-    /**
-     * Retorna o tamanho de uma posição do tabuleiro.
-     *
-     * @return
-     */
-    public int getPlaceSide()
-    {
-        return tPlaceSide;
-    }
+		tPlaceSide = getLayoutParams().width / 8;
+		tPaint.setColor(Color.BLACK);
+		tPaint.setStyle(Style.FILL);
+
+		for (int i = 0, j; i < 8; i++) {
+			for (j = i % 2; j < 8; j += 2) {
+				tPlaceRect.set(i * tPlaceSide,// left,
+						j * tPlaceSide,// top,
+						(i + 1) * tPlaceSide,// right,
+						(j + 1) * tPlaceSide// bottom
+				);
+
+				canvas.drawRect(tPlaceRect, tPaint);
+			}
+		}
+
+		tPlaceRect.set(0, 0, getLayoutParams().width, getLayoutParams().height);
+		tPaint.setStyle(Style.STROKE);
+		canvas.drawRect(tPlaceRect, tPaint);
+	}
+
+	/**
+	 * Retorna o tamanho de uma posição do tabuleiro.
+	 *
+	 * @return
+	 */
+	public int getPlaceSide() {
+		return tPlaceSide;
+	}
 }
